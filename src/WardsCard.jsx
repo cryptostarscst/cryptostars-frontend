96769f359315a729756900656d4430d196d584e2
CryptoStars CST <cryptostars.cst@gmail.com>
2025-06-04 00:46:31 -0300

    Corrigido casing definitivo para wardsCard.css

--- src/WardsCard.jsx
+++ src/WardsCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "./firebaseConfig";
-import "./styles/WardsCard.css";
+import "./styles/wardsCard.css";
 
 export default function WardsCard() {
   const [value, setValue] = useState(null);
