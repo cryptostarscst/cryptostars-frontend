e32753216b37cbab7492f03969ea814a124d72df
CryptoStars CST <cryptostars.cst@gmail.com>
2025-06-09 22:37:52 -0300

    Updated APK download link to Firebase Hosting

--- src/App.jsx
+++ src/App.jsx
@@ -160,16 +160,14 @@ function App() {
     />
   )}
 
-{isAndroid && (
-  <a
-    href="/cryptostars.apk"
-    download
-    className="btn"
-    style={{ marginTop: "20px", backgroundColor: "#00ffff", color: "#000", fontWeight: "bold" }}
-  >
-    Download 
-  </a>
-)}
+<a
+  href="https://crypto-stars-2c126.web.app/cryptostars.apk"
+  download
+  className="btn"
+  style={{ marginTop: "20px", backgroundColor: "#00ffff", color: "#000", fontWeight: "bold" }}
+>
+  Download
+</a>
 </section>
 
           {/* Seção 2 */}
